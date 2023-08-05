947343cd5eb4bea581485c06c1b63a105a4d4652
Nebojsa <nebojsap82@gmail.com>
2023-08-05 10:56:03 +0200

    Changing layout, styles, computing logic + adding favicon

--- js.js
+++ js.js
@@ -1,11 +1,6 @@
 // VARIABLES
 
 // Numbers
-let btn = document.getElementById('left').children
-let dot = document.querySelector("#right > button:nth-child(10)")
-let string
-let arr = []
-let counter = 0
 
 // Display
 let upper = document.querySelector("#upper")
@@ -13,25 +8,7 @@ let lower = document.querySelector("#lower")
 
 // FUNCTIONS, EVENTS & HANDLERS
 
-// Decimal handler
-dot.addEventListener('click', ()=> {
-    if(counter < 1 && arr.length >= 1) {
-        arr.push(dot.innerHTML)
-        counter++
-    }
-})
 
-for (let i = 0; i < btn.length; i++) {
-
-    btn[i].addEventListener("click", ()=> {
-
-    // Display numbers    
-
-    arr.push(btn[i].innerHTML)
-    
-    string = arr.join('')
-    })
-}
 
 
 
